b608fdadea49158ae05ed75f334565a4e87103ba
Akshat Rastogi <akshatrastogi6704@gmail.com>
2024-04-24 18:37:40 +0530

    reduced the dependencies

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,13 +1,20 @@
+from catboost import CatBoostClassifier
+from lightgbm import LGBMClassifier
 from sklearn.calibration import LabelEncoder
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.ensemble import AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier, IsolationForest, RandomForestClassifier
+from sklearn.linear_model import ElasticNet, Lasso, LogisticRegression, PassiveAggressiveClassifier, Perceptron, RidgeClassifier, SGDClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB, GaussianNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
 import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
+from xgboost import XGBClassifier
 
 # --server.enableXsrfProtection false
 def switch_case(argument):
@@ -18,6 +25,25 @@ def switch_case(argument):
         "Logistic Regression": LogisticRegression(),
         "Adaboost Classifier" : AdaBoostClassifier(),
         "Extra Trees Classifier" :ExtraTreeClassifier(),
+        "Gradient Boosting Classifier": GradientBoostingClassifier(),
+        "K-Nearest Neighbors Classifier": KNeighborsClassifier(),
+        "Gaussian Naive Bayes Classifier": GaussianNB(),
+        "Bernoulli Naive Bayes Classifier": BernoulliNB(),
+        "Multinomial Naive Bayes Classifier": MultinomialNB(),
+        "Passive Aggressive Classifier": PassiveAggressiveClassifier(),
+        "Ridge Classifier": RidgeClassifier(),
+        "Lasso Classifier": Lasso(),
+        "ElasticNet Classifier": ElasticNet(),
+        "Bagging Classifier": BaggingClassifier(),
+        "Stochastic Gradient Descent Classifier": SGDClassifier(),
+        "Perceptron": Perceptron(),
+        "Isolation Forest": IsolationForest(),
+        "Principal Component Analysis (PCA)": PCA(),
+        "Linear Discriminant Analysis (LDA)": LinearDiscriminantAnalysis(),
+        "Quadratic Discriminant Analysis (QDA)": QuadraticDiscriminantAnalysis(),
+        "XGBoost Classifier": XGBClassifier(),
+        "LightGBM Classifier": LGBMClassifier(),
+        "CatBoost Classifier": CatBoostClassifier()
     }
     return switcher.get(argument)
 
@@ -35,7 +61,7 @@ def main():
 
         preprocessing(df)
 
-        model_name = st.sidebar.selectbox("Select Machine Learning Model :", ["Random Forest Classifier","SVM","Decision Tree Classifier","Logistic Regression", "Adaboost Classifier","Extra Trees Classifier"])
+        model_name = st.sidebar.selectbox("Select Machine Learning Model :", ["Random Forest Classifier","SVM","Decision Tree Classifier","Logistic Regression", "Adaboost Classifier","Extra Trees Classifier","Gradient Boosting Classifier","K-Nearest Neighbors Classifier", "Gaussian Naive Bayes Classifier"])
         model = switch_case(model_name)
         run_model(df, model)
 
